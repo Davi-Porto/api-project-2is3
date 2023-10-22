51ca13c6be68dc2a9ebfe0a7d579f11ead89cb9a
Davi Porto <16davisantoss@gmail.com>
2023-10-21 21:15:34 -0300

    folder features has been created

---
+++ init.js
@@ -0,0 +1,20 @@
+function show_modal(op, id){
+    let ret=false;
+    if(op==0){ // Modal de edição do produto
+        if(id&&id!=""){
+
+            ret=true;
+        }else{
+            console.log('ID inválido');
+        }
+    }else if(op==1){ // Modal de inserção do produto
+        if(id&&id!=""){
+
+            ret=true;
+        }else{
+            console.log('ID inválido');
+        }
+        ret=true;
+    }
+    return ret;
+}
